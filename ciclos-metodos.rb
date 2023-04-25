4f77bc3c8fd2c3fb9b9b57e4397468ab0f021ac4
caesarals <slac_@hotmail.com>
2023-04-25 11:37:46 -0400

    first commit desafio evaluado 0001

---
+++ ciclos-metodos.rb
@@ -0,0 +1,19 @@
+
+# 1. Se solicita mostrar los números pares mediante un ciclo while para los números
+# comprendidos entre el 0 y el 20.
+
+while test
+    
+end
+
+
+
+
+# 2. Se solicita mostrar los números impares mediante un ciclo while para los números
+# comprendidos entre el 0 y el 20.
+
+# 3. Mostrar la tabla de multiplicar del 0 hasta el 9, para ello puedes utilizar el ciclo for o
+# while.
+
+# 4. Haciendo uso de .times o el ciclo for dibuja un triángulo con el carácter que
+# consideres más idóneo, pueden ser asteriscos (*) o numerales (#).
